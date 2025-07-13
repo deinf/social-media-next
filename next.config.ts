0c73e5ad4b23bf7bbec22db623aa235f551a66de
danang <danangekasaputra@outlook.com>
2025-07-13 23:50:34 +0700

    add base

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    base: "/friendsbook/",
 //    experimental: {
 //         reactCompiler: true,
 //         ppr: 'incremental'
